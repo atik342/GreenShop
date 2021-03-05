9b57ac4e9898b1fe6896f7852e57ee8b9178d7f4
atik342 <atikullah342@gmail.com>
2021-03-05 01:12:57 +0000

    completed adding a basic presentation layer

--- AdminSystem/OrderDataEntry.aspx.cs
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -4,11 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsOrder
+        clsOrder AnOrder = new clsOrder();
+        //capture the price
+        AnOrder.OrderDetails = txtOrderDetails.Text;
+        //store the price in the session object
+        Session["AnOrder"] = AnOrder;
+        //navgate to viewer page
+        Response.Redirect("OrderViewer.aspx");
     }
 }
